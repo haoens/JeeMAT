b64a3d5cc1f0f72d73876c0f01b0f89810db1c43
Hao En See <seehaoen88@gmail.com>
2023-01-14 23:51:03 +0800

    Education and spendings page done

--- assets/components/Spend.js
+++ assets/components/Spend.js
@@ -1,10 +1,10 @@
 import { FONTS, WIDTH, HEIGHT } from "../constants";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import Navbar from "./Navbar";
 import React, { useState } from "react";
 
-export default function Spend(){
+export default function Spend({navigation}){
 
     const [data, setData] = useState({
         dailyIncome: 80,
@@ -101,8 +101,78 @@ export default function Spend(){
                             
                         </View>
                         
+                        <View 
+                            style={{
+                                paddingHorizontal: 20,
+                                flex: 0.4,
+                                marginTop: HEIGHT * 0.05
+                            }}
+                        >
+                            <View 
+                                style={{
+                                    flex: 0.3,
+                                    flexDirection: 'row',
+                                    alignItems: 'center'
+                                }}
+                            >
+                                <Image 
+                                    source={require('../images/tip.png')}
+                                    style={{
+                                        height: 20,
+                                        width: 20,
+                                        marginRight: 6
+                                    }}
+                                />
+                                <Text 
+                                    style={{
+                                        color: 'white',
+                                        fontFamily: 'Poppins-Bold',
+                                        fontSize: 16
+                                    }}
+                                >
+                                    Saving Tip
+                                </Text>
+                            </View>
+                            
+
+                            <View style={{
+                                flex: 1,
+                                marginBottom: 5,
+                                padding: 5,
+                            }}>
+                                
+                                <Text
+                                    style={{
+                                        fontSize: 12,
+                                        fontFamily: 'Poppins-Medium',
+                                        color: 'white'
+                                    }}>
+                                    When tempted by a nonessential purchase, wait a few days. You may realize the item was something you wanted rather than needed—and you can develop a plan to save for it.
+                                </Text>
+                            </View>
+                            
+                            <TouchableOpacity style={styles.startWrapper}>
+                                <LinearGradient 
+                                    colors={['#ff739d', '#c375ff', '#7986ff']}
+                                    locations={[.3, .67, 1]}
+                                    start={{x:0, y:1}} end={{x:1, y:0}} 
+                                    style={styles.startWrapper1} 
+                                    onTouchEnd={() => navigation.navigate('Tabs')}
+                                >
+                                    <Text 
+                                        style={{
+                                            fontFamily: 'Poppins-Medium'
+                                        }}
+                                    >
+                                        Back To Home
+                                    </Text>
+                                </LinearGradient>
+                            </TouchableOpacity>
+                        </View>
 
                     </View>
+
+                    
                 
                 </LinearGradient>
             </LinearGradient>
@@ -150,5 +220,20 @@ const styles = StyleSheet.create({
         height: 50,
         backgroundColor: 'rgba(33, 161, 38, 1)',
         borderRadius: 20
+    },
+    startWrapper: {
+        flex: 0.7,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        borderRadius: WIDTH * 0.04,
+        marginRight: WIDTH * 0.02,
+    },
+    startWrapper1: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: WIDTH * 0.04,
+        paddingHorizontal: WIDTH * 0.1
+        // marginRight: WIDTH * 0.02
     },
 })
--- assets/components/Tabs.js
+++ assets/components/Tabs.js
@@ -1,5 +1,6 @@
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import Account from './Account';
+import Education from './Education';
 import Jeemat from './Jeemat';
 
 const Tab = createMaterialTopTabNavigator();
@@ -7,6 +8,7 @@ const Tab = createMaterialTopTabNavigator();
 export default function Tabs() {
   return (
     <Tab.Navigator initialRouteName='Jeemat' screenOptions={{tabBarStyle: {display:'none'}}}>
+      <Tab.Screen name="Education" component={Education} />
       <Tab.Screen name="Jeemat" component={Jeemat} />
       <Tab.Screen name="Account" component={Account} />
     </Tab.Navigator>
